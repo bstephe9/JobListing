d2102df40142c808c2614868ea9f78d021783138
bstephe9 <bstephenson9807@gmail.com>
2025-06-29 19:54:25 -0400

    Enable Swagger API

---
+++ backend/src/main/java/com/ben/joblisting/controller/PostController.java
@@ -0,0 +1,19 @@
+package com.ben.joblisting.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class PostController {
+
+	@Hidden
+	@RequestMapping("/")
+	public void redirect(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/swagger-ui.html");
+	}
+
+}
