b1198cf03beedbfedc5df116b9266323ca9e7f70
bstephe9 <bstephenson9807@gmail.com>
2025-06-29 21:45:09 -0400

    Add GET method for all posts

--- backend/src/main/java/com/ben/joblisting/controller/PostController.java
+++ backend/src/main/java/com/ben/joblisting/controller/PostController.java
@@ -1,19 +1,34 @@
 package com.ben.joblisting.controller;
 
+import com.ben.joblisting.model.Post;
+import com.ben.joblisting.repository.PostRepository;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class PostController {
+
+	PostRepository repository;
+
+	public PostController(PostRepository repository) {
+		this.repository = repository;
+	}
 
 	@Hidden
 	@RequestMapping("/")
 	public void redirect(HttpServletResponse response) throws IOException {
 		response.sendRedirect("/swagger-ui.html");
+	}
+
+	@GetMapping("/posts")
+	public List<Post> getAllPosts() {
+		return repository.findAll();
 	}
 
 }
